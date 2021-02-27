4d76b73b57bc7f6e3d40dbafbadd04f9960dae3b
yuanrra <jrblanes@hotmail.com>
2021-02-27 11:22:19 +0100

    cambio 2702

--- WindowsFormsApp13/Form1.cs
+++ WindowsFormsApp13/Form1.cs
@@ -24,11 +24,25 @@ namespace WindowsFormsApp13
         private void button1_Click(object sender, EventArgs e)
         {
             Alumno miAlumno = new Alumno();
-            String miAlumnoStr;
-
+            String miAlumnoStr, miAlumnoNotaTexto;
             miAlumno.Nombre = aluNombre.Text;
             miAlumno.Nota = Convert.ToInt32(aluNota.Text);
-            miAlumnoStr = aluNombre.Text + " " + aluNota.Text + (miAlumno.Aprobado ? " Aprobado" : " Suspenso") + "\n";
+            if (miAlumno.Nota < 5)
+            {
+                miAlumnoNotaTexto = "Suspenso";
+            }
+            else if (miAlumno.Nota < 7)
+            {
+                miAlumnoNotaTexto = "Aprobado";
+            }
+            else if (miAlumno.Nota < 9)
+            {
+                miAlumnoNotaTexto = "Notable";
+            }
+            else
+                miAlumnoNotaTexto = "Sobresaliente";
+            miAlumnoStr = aluNombre.Text + " " + aluNota.Text + " " +
+            miAlumnoNotaTexto + "\n";
             listaAlumnos.AppendText(miAlumnoStr);
             misAlumnos.Agregar(miAlumno);
         }
